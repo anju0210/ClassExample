502e9ca6a13fcbab42f3941ab7d56049173914f8
anju0210 <s2309@e-mirim.hs.kr>
2025-06-12 14:05:45 +0900

    상속 시 생성자 예제

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace ClassExample
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(int name) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string name) { Console.WriteLine("Parent(string param)"); }
+        }
+        class Child : Parent
+        {
+            public Child() : base("10")
+            {
+                Console.WriteLine("Child() : base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child(string input) : base(input)");
+            }
+        }
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
         }
     }
 }
